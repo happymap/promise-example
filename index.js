8ef382adc4e4c790b6d72d7f10e56a5b694caf3b
happymap <nafan628@gmail.com>
2016-05-26 02:16:45 -0700

    improve example

--- index.js
+++ index.js
@@ -13,6 +13,26 @@ var readFile = function (filePath) {
 	});
 }
 
+// nested callback functions
+var readFile2 = function (filePath1, filePath2) { 
+	return new Promise(function(resolve, reject) {
+		fs.readFile(filePath1, 'utf8', function(error1, result1) {
+			if (error1) {
+				reject(error1);
+			} else {
+				fs.readFile(filePath2, 'utf8', function(error2, result2) {
+					if (error2) {
+						reject(error2);
+					} else {
+						resolve(result1 + ', ' + result2);
+					}
+				});
+			}
+		});
+	});
+} 
+
+
 readFile('./hello.txt').then(function(result) {
 	console.log(result);
 	return readFile('./hello1.txt');
@@ -22,4 +42,11 @@ readFile('./hello.txt').then(function(result) {
 }).then (function(result) {
 	console.log(result);
 });
+
+// readFile2('./hello.txt', './hello1.txt').then(function(result) {
+// 	console.log(result);
+// 	return readFile2('./hello1.txt', './hello2.txt');
+// }).then(function(result) {
+// 	console.log(result);
+// });
 
