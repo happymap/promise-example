ef3373ef74d802e08eb5f620fb089d12133fc866
Yifan Ying <yying@fitbit.com>
2016-05-26 11:59:51 -0700

    add example of promise.all

--- index.js
+++ index.js
@@ -6,31 +6,30 @@ var readFile = function (filePath) {
 		fs.readFile(filePath, 'utf8', function(error, result) {
 			if (error) {
 				reject(error);
+			} 
+			var iterations = [];
+			iterations.push(fakeSonicd('./sonicd.txt'));
+			iterations.push(fakeSonicd('./sonicd2.txt'));
+
+			Promise.all(iterations).then(function(results) {
+				console.log(results);
+				resolve(result);
+			});
+		});
+	});
+}
+
+var fakeSonicd = function(filePath) {
+	return new Promise(function(resolve, reject) {
+		fs.readFile(filePath, 'utf8', function(error, result) {
+			if (error) {
+				reject(error);
 			} else {
 				resolve(result);
 			}
 		});
 	});
 }
-
-// nested callback functions
-var readFile2 = function (filePath1, filePath2) { 
-	return new Promise(function(resolve, reject) {
-		fs.readFile(filePath1, 'utf8', function(error1, result1) {
-			if (error1) {
-				reject(error1);
-			} else {
-				fs.readFile(filePath2, 'utf8', function(error2, result2) {
-					if (error2) {
-						reject(error2);
-					} else {
-						resolve(result1 + ', ' + result2);
-					}
-				});
-			}
-		});
-	});
-} 
 
 
 readFile('./hello.txt').then(function(result) {
@@ -42,11 +41,4 @@ readFile('./hello.txt').then(function(result) {
 }).then (function(result) {
 	console.log(result);
 });
-
-// readFile2('./hello.txt', './hello1.txt').then(function(result) {
-// 	console.log(result);
-// 	return readFile2('./hello1.txt', './hello2.txt');
-// }).then(function(result) {
-// 	console.log(result);
-// });
 
